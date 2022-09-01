b8945e64b46752b4d507e2de51d5e7ce013b44b8
Timothy Gresham <GRESHAMT@ETSU.EDU>
2022-09-01 17:48:31 -0400

    Finished question 2

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http.Headers;
+
 Console.WriteLine("Hello, World!");
 
 // 1. Printting the output of adding two number from the user input.
@@ -9,4 +11,19 @@ Console.WriteLine("Please enter a number");
 int num2 = Int32.Parse(Console.ReadLine());
 int total = num1 + num2;
 Console.WriteLine("Your total is: " + total);
-/////
+
+// 2. Take a user's input number and input its multiplication table.
+Console.WriteLine("Enter the number you want a multiplication table for.");
+int num3 = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Enter how high you want it to multiply by.");
+int num4 = int.Parse(Console.ReadLine());
+int product;
+
+Console.WriteLine("\nMultiplication table for {0}: ", num3);
+for (int i = 1; i <= num4; i++)
+{
+    product = num3 * i;
+    Console.WriteLine("{0} x {1} = {2}", num3, i, product);
+}
+
+            
